e90af29bf9303387b49462a7f698778eb989567c
Thibault BRONCHAIN <thibault@bronchain.me>
2015-02-26 19:26:17 +0800

    test mod service2

--- sources/mod/linux.py
+++ sources/mod/linux.py
@@ -584,12 +584,15 @@ ensure the specified services are running, and trigger service restart if necess
 
 *   **`name`** (*required*): the list of the service names to be run. The value represents the action if one of the watched file has changed (if applicable):
 
+
+		example: httpd: bash /opt/scripts/nginx_mysql_restart.sh, mysqld: restart
+
+
 	- ***`<null>`*** *`default`*: try to reload the service. If reload fails, restart the service.
 	- ***`<command>`***: path to a command to execute
 	- ***`reload`***: 'reload' the service
 	- ***`restart`***: 'restart' the service
 
-		example: httpd: bash /opt/scripts/nginx_mysql_restart.sh, mysqld: restart
 
 *   **`watch`** (*optional*): watch a list of files, execute action if any of them is modified
 
