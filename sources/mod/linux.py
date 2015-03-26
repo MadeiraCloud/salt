e4f2386b193cfbf4e7b0d6c2f753f238f7c71f51
Thibault BRONCHAIN <thibault@bronchain.me>
2015-03-26 16:47:45 +0800

    fix modules

--- sources/mod/linux.py
+++ sources/mod/linux.py
@@ -2732,7 +2732,7 @@ Runs a Mesos Master instance
 				},
 				"framework"	: {
 					"type"		: "array",
-					"required"	: True,
+					"required"	: False,
 					"visible"	: True,
 					"option"	: [
 						"marathon",
@@ -2750,27 +2750,27 @@ Runs a Mesos Master instance
 			"reference": {
 				"cn": "",
 				"en": """
-	### Description
-	Runs a Mesos Slave instance
+### Description
+Runs a Mesos Slave instance
 
-	### Parameters
+### Parameters
 
-	*   **`masters_addresses`** (*required*): IPs/hostnames of the Mesos Master servers. If no hostname is given, the ip address will be used by default as hostname.
+*   **`masters_addresses`** (*required*): IPs/hostnames of the Mesos Master servers. If no hostname is given, the ip address will be used by default as hostname.
 
-			example:
-				@{master1.PrivateIpAddress}:    master1
-				@{master2.PrivateIpAddress}:    master2
-				@{master3.PrivateIpAddress}:
+		example:
+			@{master1.PrivateIpAddress}:    master1
+			@{master2.PrivateIpAddress}:    master2
+			@{master3.PrivateIpAddress}:
 
-	*   **`attributes`** (*required*): Mesos slave attributes (see Mesos documentation)
+*   **`attributes`** (*required*): Mesos slave attributes (see Mesos documentation)
 
-			example:
-				subnet: web
-				public: true
-				rack:   rack-5
-				asg:    asg0
-				zone:   all
-	""",
+		example:
+			subnet: web
+			public: true
+			rack:   rack-5
+			asg:    asg0
+			zone:   all
+""",
 			},
 			"parameter" : {
 				"masters_addresses" : {
@@ -2789,7 +2789,7 @@ Runs a Mesos Master instance
 					"visible"	: False,
 				},
 			},
-		}
+		},
 
 	}
 }
